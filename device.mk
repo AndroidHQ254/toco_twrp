9fcdc67f6e74c9c2b644091279a8ea07baa9d6ca
AndroidHQ254 <d.murimi14@gmail.com>
2022-05-30 02:56:39 +0300

    toco: Drop unnecesary stuff
    * build/make/core/Makefile:61: error: overriding commands for target `out/target/product/toco/recovery/root/system/lib64/libandroidicu.so', previously defined at build/make/core/base_rules.mk:525
    
    * Adjust notch height while at it
    * Add BUILD_BROKEN_MISSING_REQUIRED_MODULES flag as well

--- device.mk
+++ device.mk
@@ -31,16 +31,13 @@ PRODUCT_PACKAGES += \
     fastbootd \
     android.hardware.fastboot@1.0-impl-mock
 
-TARGET_RECOVERY_DEVICE_MODULES += libion libandroidicu vendor.display.config@1.0 vendor.display.config@2.0 libdisplayconfig.qti
+TARGET_RECOVERY_DEVICE_MODULES += libion vendor.display.config@1.0 vendor.display.config@2.0 libdisplayconfig.qti
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so
-
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/toco/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
 
 # Vibrator
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
--- twrp_toco.mk
+++ twrp_toco.mk
@@ -18,7 +18,6 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
@@ -27,7 +26,6 @@ $(call inherit-product, device/xiaomi/toco/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-# $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := toco
