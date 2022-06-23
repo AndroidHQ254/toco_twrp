b88169769eadae33f87cf8725a484b8fa149c169
Captain Throwback <captainthrowback@hotmail.com>
2022-06-23 14:05:07 +0300

    toco: Build some more libs

--- device.mk
+++ device.mk
@@ -31,10 +31,15 @@ PRODUCT_PACKAGES += \
     fastbootd \
     android.hardware.fastboot@1.0-impl-mock
 
-TARGET_RECOVERY_DEVICE_MODULES += libion
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libxml2 \
+    libicuuc
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so
 
 # Properties
 TW_OVERRIDE_SYSTEM_PROPS := \
