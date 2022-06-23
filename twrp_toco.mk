543114cf0e0a1eb951ac021955b3f187abb3e456
AndroidHQ254 <d.murimi14@gmail.com>
2022-06-23 14:02:36 +0300

    toco: More updates for A12

--- twrp_toco.mk
+++ twrp_toco.mk
@@ -17,8 +17,8 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from toco device
