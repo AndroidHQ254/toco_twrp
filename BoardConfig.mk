989d38793b546b02dd66f38c9e5e77e094dc8240
AndroidHQ254 <14816738+AndroidHQ254@users.noreply.github.com>
2022-10-06 02:57:10 +0300

    toco: Change to a distinct version

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,4 +158,4 @@ TW_MAX_BRIGHTNESS := 2047
 TW_DEFAULT_BRIGHTNESS := 1200
 TW_INCLUDE_RESETPROP := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
-TW_DEVICE_VERSION := EROFS-toco-DennisMurimi
+TW_DEVICE_VERSION := A13-toco-DennisMurimi
