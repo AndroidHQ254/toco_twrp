b6a111fadbcca03f72289a908ac03a000ceea5f6
Darth9 <reyprimerjefe@gmail.com>
2023-02-21 22:21:50 +0300

    toco: Update the script messages

--- recovery/root/system/bin/post_rom_flash.sh
+++ recovery/root/system/bin/post_rom_flash.sh
@@ -1,8 +1,9 @@
 #!/system/bin/sh
 echo "I: Running post-ROM-flash script..." >> /tmp/recovery.log;
+echo "I: Arguments received=\"$@\"" >> /tmp/recovery.log;
 D=/data/system/package_cache;
 if [ -d $D ]; then 
-	echo "I: Removing a dangling directory (\"$D\") left behind by the installer..." >> /tmp/recovery.log;   
-	rm -rf $D;
+	echo "I: Removing dangling cache files (\"$D\") left behind by the installer..." >> /tmp/recovery.log;
+	rm -rf $D/*;
 fi
 exit 0;
