a2c58e42043d7bbc7094a7a43c75dc57cc0f6fba
Captain Throwback <captainthrowback@hotmail.com>
2023-09-22 19:52:50 +0300

    toco: `libxml2` is built by default

--- device.mk
+++ device.mk
@@ -33,7 +33,6 @@ PRODUCT_PACKAGES += \
 
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
-    libxml2 \
     libicuuc \
     libdisplayconfig.qti \
     libdisplayconfig.qti.vendor \
@@ -43,7 +42,6 @@ TARGET_RECOVERY_DEVICE_MODULES += \
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
