4378b25d2b51b05016728d4621a4950e6387eadf
AndroidHQ254 <d.murimi14@gmail.com>
2023-12-02 03:56:38 +0300

    toco: Exclude APEX
    * Drop `TW_DEVICE_VERSION` flag while here

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,4 +168,4 @@ TW_MAX_BRIGHTNESS := 2047
 TW_DEFAULT_BRIGHTNESS := 1200
 TW_INCLUDE_RESETPROP := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
-TW_DEVICE_VERSION := A13-toco-DennisMurimi
+TW_EXCLUDE_APEX := true
