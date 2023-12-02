485d1f847b8c26747fa378b688284af9938191d0
AndroidHQ254 <d.murimi14@gmail.com>
2023-12-02 19:58:53 +0300

    toco: Exclude APEX

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,3 +161,4 @@ TW_MAX_BRIGHTNESS := 2047
 TW_DEFAULT_BRIGHTNESS := 1200
 TW_INCLUDE_RESETPROP := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
+TW_EXCLUDE_APEX := true
